f1811ae464312f2282380f80d8e3a60ba252b1bf
sxwqsd <mercado.valeriai856@gmail.com>
2021-07-13 22:38:39 -0700

    i added the time slots

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,18 @@
-var todayDate = moment().format('dddd, MMM Do YYYY');
+var todayDate = moment().format('dddd, LL');
 $("#currentDay").html(todayDate);
+
+var currentTime = moment().format('LT');
+$("#currentTime").html(currentTime);
+
+var timeSlots = [
+    { time: "9 AM", event: "" },
+    { time: "10 AM", event: "" },
+    { time: "11 AM", event: "" },
+    { time: "12 PM", event: "" },
+    { time: "1 PM", event: "" },
+    { time: "2 PM", event: "" },
+    { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" },
+];
 
